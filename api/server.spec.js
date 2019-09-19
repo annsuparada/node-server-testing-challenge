9199303684a08037495ac43fb9630074fe59a79f
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-19 15:14:42 -0500

    test more server

--- api/server.spec.js
+++ api/server.spec.js
@@ -3,12 +3,33 @@ const server = require('./server.js');
 
 describe('server.js', () => {
     describe('GET /', () => {
-        it('return 200 OK', () => {
-            return request(server)
-                .get('/')
-                .then(res => {
-                    expect(res.status).toBe(200);
-                })
-        })
-    })
-})
+      it('returns 200 OK', () => {
+        // make a GET request to the / endpoint on the server
+        return request(server)
+          .get('/')
+          .then(res => {
+            // assert that we get an http status code 200
+            expect(res.status).toBe(200);
+          });
+      });
+  
+      it("should return { api: 'up' }", async () => {
+        const res = await request(server).get('/');
+  
+        expect(res.body.api).toBe('up');
+        expect(res.body).toEqual({ api: 'up' });
+      });
+  
+      it('returns JSON', done => {
+        request(server)
+          .get('/')
+          .then(res => {
+            // assert that we get an http status code 200
+            expect(res.type).toMatch(/json/i);
+            done();
+          });
+      });
+    });
+  });
+  
+
